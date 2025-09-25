b14b40e1c616c0d84baefa20413353d25e198bf0
codexdei <jlacostc@gmail.com>
2025-09-25 12:54:08 -0500

    se aplica Model al UserController.java

--- springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,5 +1,21 @@
 package com.codexdei.curso.springboot.webapp.springboot_web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+    //esta anotacion hace que este método se ejecute cuando alguien abre http://localhost:8080/details.
+    @GetMapping("/details")
+    //Model permite pasar datos del controlador(que es esta clase por la anotacion @Controller) a la vista
+    public String details(Model model){
+
+        model.addAttribute("title", "Hello World!!");
+        model.addAttribute("name", "Yorking");
+        model.addAttribute("lastname", "Master");
+
+        return "details";
+    }
 }
