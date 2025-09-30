e4438e76cefa539180eb52c5ede07f6875d8da77
codexdei <jlacostc@gmail.com>
2025-09-30 11:10:07 -0500

    se implementa thymeleaf

--- springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.codexdei.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.codexdei.curso.springboot.webapp.springboot_web.models.User;
@@ -16,9 +20,20 @@ public class UserController {
 
         User user = new User("Yorking", "Master");
 
+
+        user.setEmail("yorking@sion.celest");
         model.addAttribute("title", "Hello World!!");
         model.addAttribute("user", user);
-
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+
+        return "list";
     }
 }
--- springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/codexdei/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.codexdei.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codexdei.curso.springboot.webapp.springboot_web.models.User;
+import com.codexdei.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,11 +28,23 @@ O un modelo con datos que la vista mostrará.
 @RequestMapping("/api")
 public class UserRestController {
 
-    //esta anotacion hace que este método se ejecute cuando alguien abre http://localhost:8080/details.
-    //Lo siguiente es equivalente a @GetMapping("/details")
-    //@RequestMapping(path = "details", method=RequestMethod.GET)
     @GetMapping("/details")
-    public Map<String,Object> details(){
+    public UserDto details(){
+
+        User user = new User("Yorking", "Master");
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hello Wordl");
+
+        return userDto;
+    }
+
+    //esta anotacion hace que este método se ejecute cuando alguien abre http://localhost:8080/details.
+    //Lo siguiente es equivalente a:
+    // @GetMapping("/details-map") @RequestMapping(path = "details", method=RequestMethod.GET)
+    @GetMapping("/details-map")
+    public Map<String,Object> detailsMap(){
 
         User user = new User("Yorking", "Master");
         Map<String,Object> body = new HashMap<>();
@@ -37,5 +53,23 @@ public class UserRestController {
         body.put("user", user);
 
         return body;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+
+        
+        User user = new User("Yorking", "Master");
+        User user2 = new User("Legolas", "Mon");
+        User user3 = new User("Ades", "Mon");
+
+        List<User> users = Arrays.asList(user,user2,user3);
+    /*    
+        ArrayList users = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+        userList.add(user3);
+    */
+        return users;
     }
 }
